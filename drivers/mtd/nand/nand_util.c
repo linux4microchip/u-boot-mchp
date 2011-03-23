efec106460326e3c9685728ada4cb40012fbce6d
Nicolas Ferre <nicolas.ferre@atmel.com>
2011-03-23 17:40:22 +0800

    nand util: read/write: accept unaligned length
    Backported from current u-boot.
    
    An unaligned length, though, is useful for writing jffs2 images
    without modifying underlying 0xff at the end of a page/block.
    
    Signed-off-by: Nicolas Ferre <nicolas.ferre@atmel.com>

--- drivers/mtd/nand/nand_util.c
+++ drivers/mtd/nand/nand_util.c
@@ -478,8 +478,7 @@ int nand_write_skip_bad(nand_info_t *nand, loff_t offset, size_t *length,
 	u_char *p_buffer = buffer;
 
 	/* Reject writes, which are not page aligned */
-	if ((offset & (nand->writesize - 1)) != 0 ||
-	    (*length & (nand->writesize - 1)) != 0) {
+	if ((offset & (nand->writesize - 1)) != 0) {
 		printf ("Attempt to write non page aligned data\n");
 		return -EINVAL;
 	}
