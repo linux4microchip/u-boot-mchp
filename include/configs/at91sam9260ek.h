511f86ff247cda36a209676bce95486587545a8b
Nicolas Ferre <nicolas.ferre@atmel.com>
2013-02-18 16:34:07 +0100

    at91/configs: at91sam9260/9g20: add libfdt and bootz to configuration
    Signed-off-by: Nicolas Ferre <nicolas.ferre@atmel.com>

--- include/configs/at91sam9260ek.h
+++ include/configs/at91sam9260ek.h
@@ -62,6 +62,9 @@
 #define CONFIG_BOARD_EARLY_INIT_F
 #define CONFIG_DISPLAY_CPUINFO
 
+#define CONFIG_CMD_BOOTZ
+#define CONFIG_OF_LIBFDT
+
 /* general purpose I/O */
 #define CONFIG_ATMEL_LEGACY		/* required until (g)pio is fixed */
 #define CONFIG_AT91_GPIO
