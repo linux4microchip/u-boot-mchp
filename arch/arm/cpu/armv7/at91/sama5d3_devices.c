675596c86b9eda8d823f7221f7df936f546b30ed
Wu, Josh <Josh.wu@atmel.com>
2013-11-05 15:07:46 +0800

    ARM: at91: sama5d3: add support for sama5d36 chip
    The SAMA5D36 chip is the superset product of SAMA5D3x family.
    
    For detail information please refer to:
      http://www.atmel.com/Microsite/sama5d3/default.aspx
    
    Signed-off-by: Josh Wu <josh.wu@atmel.com>
    Acked-by: Bo Shen <voice.shen@atmel.com>
    Signed-off-by: Andreas Bießmann <andreas.devel@googlemail.com>

--- arch/arm/cpu/armv7/at91/sama5d3_devices.c
+++ arch/arm/cpu/armv7/at91/sama5d3_devices.c
@@ -31,7 +31,7 @@
 
 unsigned int has_emac()
 {
-	return cpu_is_sama5d31() || cpu_is_sama5d35();
+	return cpu_is_sama5d31() || cpu_is_sama5d35() || cpu_is_sama5d36();
 }
 
 unsigned int has_gmac()
@@ -58,6 +58,8 @@ char *get_cpu_name()
 			return "SAMA5D34";
 		case ARCH_EXID_SAMA5D35:
 			return "SAMA5D35";
+		case ARCH_EXID_SAMA5D36:
+			return "SAMA5D36";
 		default:
 			return "Unknown CPU type";
 		}
