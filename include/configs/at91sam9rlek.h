373bcfcbc2cd1592f25aba59e71d7bcf4d462b65
Wenyou Yang <wenyou.yang@atmel.com>
2017-02-24 15:33:35 +0800

    configs: at91sam9rlek: Update for DT and DM support
    Update the configuration files to support the device tree and driver
    model. The device clock and pins configuration are handled by the
    clock and the pinctrl drivers respectively.
    
    Signed-off-by: Wenyou Yang <wenyou.yang@atmel.com>

--- include/configs/at91sam9rlek.h
+++ include/configs/at91sam9rlek.h
@@ -29,17 +29,12 @@
 #define CONFIG_INITRD_TAG		1
 
 #define CONFIG_ATMEL_LEGACY
-#define CONFIG_AT91_GPIO		1
-#define CONFIG_AT91_GPIO_PULLUP		1
 
 /*
  * Hardware drivers
  */
 
 /* serial console */
-#define CONFIG_ATMEL_USART
-#define CONFIG_USART_BASE		ATMEL_BASE_DBGU
-#define CONFIG_USART_ID			ATMEL_ID_SYS
 #define CONFIG_BAUDRATE			115200
 
 /* LCD */
@@ -54,13 +49,6 @@
 /* Let board_init_f handle the framebuffer allocation */
 #undef CONFIG_FB_ADDR
 
-/* LED */
-#define CONFIG_AT91_LED
-#define	CONFIG_RED_LED		AT91_PIN_PD14	/* this is the power led */
-#define	CONFIG_GREEN_LED	AT91_PIN_PD15	/* this is the user1 led */
-#define	CONFIG_YELLOW_LED	AT91_PIN_PD16	/* this is the user2 led */
-
-
 /*
  * Command line configuration.
  */
@@ -73,7 +61,7 @@
 #define CONFIG_SYS_SDRAM_SIZE		0x04000000
 
 #define CONFIG_SYS_INIT_SP_ADDR \
-	(ATMEL_BASE_SRAM + 0x1000 - GENERATED_GBL_DATA_SIZE)
+	(ATMEL_BASE_SRAM + 16 * 1024 - GENERATED_GBL_DATA_SIZE)
 
 /* DataFlash */
 #define CONFIG_ATMEL_DATAFLASH_SPI
@@ -97,12 +85,6 @@
 #define CONFIG_SYS_NAND_ENABLE_PIN		AT91_PIN_PB6
 #define CONFIG_SYS_NAND_READY_PIN		AT91_PIN_PD17
 
-#endif
-
-/* MMC */
-
-#ifdef CONFIG_CMD_MMC
-#define CONFIG_GENERIC_ATMEL_MCI
 #endif
 
 /* Ethernet - not present */
