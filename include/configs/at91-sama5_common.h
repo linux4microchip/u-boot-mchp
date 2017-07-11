7b9f817cf9d9bfd8ccff57d23b2b2fec46b8f01e
Wenyou Yang <wenyou.yang@microchip.com>
2017-07-11 10:23:25 +0800

    configs: at91-sama5_common: QSPI: Adjust BOOTCOMMAND and ENV_OFFSET
    According to the new QSPIflash memory layout, adjust the BOOTCOMMAND
    and ENV_OFFSET variable value.
    
    Signed-off-by: Wenyou Yang <wenyou.yang@microchip.com>

--- include/configs/at91-sama5_common.h
+++ include/configs/at91-sama5_common.h
@@ -91,12 +91,12 @@
 /* u-boot env in serial flash, by default is bus 0 and cs 0 */
 #define CONFIG_ENV_IS_IN_SPI_FLASH
 #ifdef CONFIG_QSPI_BOOT
-#define CONFIG_ENV_OFFSET		0x10000
+#define CONFIG_ENV_OFFSET		0xb0000
 #define CONFIG_ENV_SIZE			0x10000
 #define CONFIG_ENV_SECT_SIZE		0x10000
 #define CONFIG_BOOTCOMMAND		"sf probe 0; "				\
-					"sf read 0x21000000 0x60000 0x10000; "	\
-					"sf read 0x22000000 0x70000 0x500000; "	\
+					"sf read 0x21000000 0xc0000 0x20000; "	\
+					"sf read 0x22000000 0xe0000 0x400000; "	\
 					"bootz 0x22000000 - 0x21000000"
 #else
 #define CONFIG_ENV_OFFSET		0x6000
