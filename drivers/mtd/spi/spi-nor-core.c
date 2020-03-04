125bd26a4f698fa69f018676e958ce434feced16
Tudor Ambarus <tudor.ambarus@microchip.com>
2020-03-04 14:47:59 +0200

    [not upstreamable] mtd: spi-nor: add Global Block Unlock support
    Proper fix would be to add support for individual block lock protection
    and to use the global block unlock support as an optimization when we
    want to unlock all the individual blocks.
    
    We can't determine this purely by manufacturer type and it's not
    autodetectable by anything like SFDP, so make a new flag for it:
    UNLOCK_GLOBAL_BLOCK.
    
    Note that the Global Block Unlock command has different names
    depending on the manufacturer, but always the same command value:
    0x98. Macronix's MX25U12835F names it Gang Block Unlock,
    Winbound's W25Q128FV names it Global Block Unlock and
    Microchip's SST26VF064B names it Global Block Protection Unlock.
    
    Signed-off-by: Tudor Ambarus <tudor.ambarus@microchip.com>
    [eugen.hristev@microchip.com: Ported to 2021.01]
    Signed-off-by: Eugen Hristev <eugen.hristev@microchip.com>
    [eugen.hristev@microchip.com: Ported to 2022.01]
    Signed-off-by: Eugen Hristev <eugen.hristev@microchip.com>

--- drivers/mtd/spi/spi-nor-core.c
+++ drivers/mtd/spi/spi-nor-core.c
@@ -3133,7 +3133,10 @@ static int spi_nor_select_erase(struct spi_nor *nor,
 		mtd->erasesize = 4096;
 	} else
 #endif
-	{
+	if (info->flags & SECT_4K_ONLY) {
+		nor->erase_opcode = SPINOR_OP_BE_4K;
+		mtd->erasesize = 4096;
+	} else {
 		nor->erase_opcode = SPINOR_OP_SE;
 		mtd->erasesize = info->sector_size;
 	}
@@ -3722,9 +3725,31 @@ static int spi_nor_octal_dtr_enable(struct spi_nor *nor)
 	return 0;
 }
 
+static int spi_nor_unlock_global_block_protection(struct spi_nor *nor)
+{
+	int ret;
+
+	write_enable(nor);
+
+	ret = nor->write_reg(nor, SPINOR_OP_GBULK, NULL, 0);
+	if (ret < 0)
+		return ret;
+
+	return spi_nor_wait_till_ready(nor);
+}
+
 static int spi_nor_init(struct spi_nor *nor)
 {
 	int err;
+
+	if (nor->info->flags & UNLOCK_GLOBAL_BLOCK) {
+		err = spi_nor_unlock_global_block_protection(nor);
+		if (err) {
+			dev_err(nor->dev,
+				"Cannot unlock the global block protection\n");
+			return err;
+		}
+	}
 
 	err = spi_nor_octal_dtr_enable(nor);
 	if (err) {
