b094ab47d2e57f24caac93d96f3b4b48b13e21e9
Claudiu Beznea <claudiu.beznea@microchip.com>
2020-06-02 10:06:52 +0300

    clk: add pre-requisite headers for AT91 clock architecture
    Add pre-requisite headers for AT91 clock architecture. These
    are based on already present files on Linux and will be used
    by following commits for AT91 CCF clock drivers.
    
    Signed-off-by: Claudiu Beznea <claudiu.beznea@microchip.com>

---
+++ include/dt-bindings/clk/at91.h
@@ -0,0 +1,19 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+/*
+ * This header provides constants for AT91 pmc status.
+ *
+ * The constants defined in this header are being used in dts and PMC code.
+ *
+ * Based on at91.h on Linux.
+ */
+
+#ifndef _DT_BINDINGS_CLK_AT91_H
+#define _DT_BINDINGS_CLK_AT91_H
+
+#define PMC_TYPE_CORE		1
+#define PMC_TYPE_SYSTEM		2
+#define PMC_TYPE_PERIPHERAL	3
+#define PMC_TYPE_GCK		4
+#define PMC_TYPE_SLOW		5
+
+#endif
