66c49d366d09639168906946c9a00012a58a31fb
Claudiu Beznea <claudiu.beznea@microchip.com>
2021-07-23 11:19:40 +0300

    clk: at91: sckc: add support for sama5d4 slow clock
    Add support for SAMA5D4 slow clock. This support will be used
    in SAMA5D2 CCF implementation.
    
    Signed-off-by: Claudiu Beznea <claudiu.beznea@microchip.com>
    Signed-off-by: Tudor Ambarus <tudor.ambarus@microchip.com>

--- drivers/clk/at91/sckc.c
+++ drivers/clk/at91/sckc.c
@@ -16,16 +16,15 @@
 #include "pmc.h"
 
 #define UBOOT_DM_CLK_AT91_SAM9X60_TD_SLCK	"at91-sam9x60-td-slck"
+#define UBOOT_DM_CLK_AT91_SAMA5D4_TD_SLCK	"at91-sama5d4-sckc"
 #define UBOOT_DM_CLK_AT91_SCKC			"at91-sckc"
-
-#define AT91_OSC_SEL		BIT(24)
-#define AT91_OSC_SEL_SHIFT	(24)
 
 struct sam9x60_sckc {
 	void __iomem *reg;
 	const char **parent_names;
 	unsigned int num_parents;
 	struct clk clk;
+	u8 sel;
 };
 
 #define to_sam9x60_sckc(c)	container_of(c, struct sam9x60_sckc, clk)
@@ -60,7 +59,7 @@ static int sam9x60_td_slck_set_parent(struct clk *clk, struct clk *parent)
 	if (i == sckc->num_parents)
 		return -EINVAL;
 
-	pmc_update_bits(sckc->reg, 0, AT91_OSC_SEL, (i << AT91_OSC_SEL_SHIFT));
+	pmc_update_bits(sckc->reg, 0, BIT(sckc->sel), (i << sckc->sel));
 
 	return 0;
 }
@@ -93,9 +92,10 @@ static struct clk *at91_sam9x60_clk_register_td_slck(struct sam9x60_sckc *sckc,
 			goto free;
 	}
 	sckc->num_parents = num_parents;
+	sckc->sel = 24;
 
 	pmc_read(sckc->reg, 0, &val);
-	val = (val & AT91_OSC_SEL) >> AT91_OSC_SEL_SHIFT;
+	val = (val & BIT(sckc->sel)) >> sckc->sel;
 
 	clk = &sckc->clk;
 	ret = clk_register(clk, UBOOT_DM_CLK_AT91_SAM9X60_TD_SLCK, name,
@@ -168,5 +168,92 @@ U_BOOT_DRIVER(at91_sckc) = {
 	.priv_auto	= sizeof(struct sam9x60_sckc) * 2,
 	.ops = &sam9x60_sckc_ops,
 	.probe = at91_sam9x60_sckc_probe,
+	.flags = DM_FLAG_PRE_RELOC,
+};
+
+static int sama5d4_sckc_of_xlate(struct clk *clk,
+				 struct ofnode_phandle_args *args)
+{
+	clk->id = AT91_TO_CLK_ID(PMC_TYPE_SLOW, 0);
+
+	return 0;
+}
+
+static const struct clk_ops sama5d4_slck_ops = {
+	.of_xlate = sama5d4_sckc_of_xlate,
+	.get_rate = clk_generic_get_rate,
+	.set_parent = sam9x60_td_slck_set_parent,
+};
+
+static int at91_sama5d4_sckc_probe(struct udevice *dev)
+{
+	struct sam9x60_sckc *sckc = dev_get_priv(dev);
+	void __iomem *base = (void *)devfdt_get_addr(dev);
+	const char *slow_rc_osc, *slow_osc;
+	struct clk *clk, c;
+	u32 val;
+	int ret;
+
+	ret = clk_get_by_index(dev, 0, &c);
+	if (ret)
+		return ret;
+	slow_rc_osc = clk_hw_get_name(&c);
+
+	ret = clk_get_by_index(dev, 1, &c);
+	if (ret)
+		return ret;
+	slow_osc = clk_hw_get_name(&c);
+
+	sckc->parent_names = kzalloc(sizeof(*sckc->parent_names) * 2,
+				     GFP_KERNEL);
+	if (!sckc->parent_names)
+		return -ENOMEM;
+
+	sckc->parent_names[0] = kmemdup(slow_rc_osc, strlen(slow_rc_osc) + 1,
+					GFP_KERNEL);
+	sckc->parent_names[1] = kmemdup(slow_osc, strlen(slow_osc) + 1,
+					GFP_KERNEL);
+	if (!sckc->parent_names[0] || !sckc->parent_names[1]) {
+		ret = -ENOMEM;
+		goto free;
+	}
+
+	sckc->num_parents = 2;
+	sckc->reg = base;
+	sckc->sel = 3;
+
+	pmc_read(sckc->reg, 0, &val);
+	val = (val & BIT(sckc->sel)) >> sckc->sel;
+
+	clk = &sckc->clk;
+	ret = clk_register(clk, UBOOT_DM_CLK_AT91_SAM9X60_TD_SLCK, "slowck",
+			   sckc->parent_names[val]);
+	if (ret)
+		goto free;
+
+	clk_dm(AT91_TO_CLK_ID(PMC_TYPE_SLOW, 0), clk);
+
+	return 0;
+
+free:
+	kfree(sckc->parent_names[0]);
+	kfree(sckc->parent_names[1]);
+	kfree(sckc->parent_names);
+
+	return ret;
+}
+
+static const struct udevice_id sama5d4_sckc_ids[] = {
+	{ .compatible = "atmel,sama5d4-sckc" },
+	{ /* Sentinel. */ },
+};
+
+U_BOOT_DRIVER(at91_sama5d4_ckc) = {
+	.name = UBOOT_DM_CLK_AT91_SCKC,
+	.id = UCLASS_CLK,
+	.of_match = sama5d4_sckc_ids,
+	.priv_auto	= sizeof(struct sam9x60_sckc),
+	.ops = &sama5d4_slck_ops,
+	.probe = at91_sama5d4_sckc_probe,
 	.flags = DM_FLAG_PRE_RELOC,
 };
