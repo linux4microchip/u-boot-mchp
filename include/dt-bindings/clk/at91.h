867e67aeec465464de0b6d2d84ab5ca922e65d20
Claudiu Beznea <claudiu.beznea@microchip.com>
2021-07-23 11:37:33 +0300

    clk: at91: sama5d2: add clock support compatible with ccf
    Add clock support for SAMA5D2 compatible with common clock
    framework.
    
    Signed-off-by: Claudiu Beznea <claudiu.beznea@microchip.com>
    Signed-off-by: Tudor Ambarus <tudor.ambarus@microchip.com>

--- include/dt-bindings/clk/at91.h
+++ include/dt-bindings/clk/at91.h
@@ -19,4 +19,38 @@
 #define PMC_TYPE_GCK		4
 #define PMC_TYPE_SLOW		5
 
+/* Per SoC core clock identifiers. */
+#ifdef CONFIG_SAMA5D2
+#define ID_SLCK			1
+#define ID_MAIN_XTAL		2
+#define ID_MAIN_RC		3
+#define ID_MAIN_RC_OSC		4
+#define ID_MAIN_OSC		5
+#define ID_MAINCK		6
+
+#define ID_PLLACK		7
+#define ID_PLLADIVCK		8
+#define ID_MCK_DIV		9
+#define ID_MCK_PRES		10
+
+#define ID_H32MX		11
+
+#define ID_UTMI			12
+
+#define ID_PROG0		13
+#define ID_PROG1		14
+#define ID_PROG2		15
+
+#define ID_PCK0			16
+#define ID_PCK1			17
+#define ID_PCK2			18
+
+#define ID_DDR			19
+#define ID_LCD			20
+#define ID_USBH			21
+#define ID_USBD			22
+#define ID_ISCCK		23
+
+#endif
+
 #endif
