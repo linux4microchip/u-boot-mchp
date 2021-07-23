8d3f54aaf565fcaab3cb3217c517fb93ed6d9e66
Claudiu Beznea <claudiu.beznea@microchip.com>
2021-07-23 10:56:11 +0300

    clk: at91: clk-generic: allow having continuous mux table
    For generic clocks with continuous mux tables there is no need to
    have both, mux_table and clk_mux_table. Only clk_mux_table is
    needed to make the translation b/w driver and core logic.
    
    Signed-off-by: Claudiu Beznea <claudiu.beznea@microchip.com>
    Signed-off-by: Tudor Ambarus <tudor.ambarus@microchip.com>

--- drivers/clk/at91/clk-generic.c
+++ drivers/clk/at91/clk-generic.c
@@ -69,10 +69,12 @@ static int clk_gck_set_parent(struct clk *clk, struct clk *parent)
 	if (index < 0)
 		return index;
 
-	index = at91_clk_mux_index_to_val(gck->mux_table, gck->num_parents,
-					  index);
-	if (index < 0)
-		return index;
+	if (gck->mux_table) {
+		index = at91_clk_mux_index_to_val(gck->mux_table,
+						  gck->num_parents, index);
+		if (index < 0)
+			return index;
+	}
 
 	pmc_write(gck->base, gck->layout->offset,
 		  (gck->id & gck->layout->pid_mask));
@@ -151,7 +153,7 @@ at91_clk_register_generic(void __iomem *base,
 	u32 val;
 
 	if (!base || !layout || !name || !parent_names || !num_parents ||
-	    !clk_mux_table || !mux_table || !range)
+	    !clk_mux_table || !range)
 		return ERR_PTR(-EINVAL);
 
 	gck = kzalloc(sizeof(*gck), GFP_KERNEL);
@@ -176,11 +178,15 @@ at91_clk_register_generic(void __iomem *base,
 	val = (val & gck->layout->gckcss_mask) >>
 		(ffs(gck->layout->gckcss_mask) - 1);
 
-	index = at91_clk_mux_val_to_index(gck->mux_table, gck->num_parents,
-					  val);
-	if (index < 0) {
-		kfree(gck);
-		return ERR_PTR(index);
+	if (gck->mux_table) {
+		index = at91_clk_mux_val_to_index(gck->mux_table,
+						  gck->num_parents, val);
+		if (index < 0) {
+			kfree(gck);
+			return ERR_PTR(index);
+		}
+	} else {
+		index = val;
 	}
 
 	ret = clk_register(clk, UBOOT_DM_CLK_AT91_GCK, name,
