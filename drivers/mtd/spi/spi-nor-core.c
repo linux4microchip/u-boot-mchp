a0054d27b7562f32df55ceae627b1e8ffdaa5901
Tudor Ambarus <tudor.ambarus@microchip.com>
2022-02-23 14:51:06 +0200

    mtd: spi-nor: core: Allow specifying the byte order in DTR mode
    Macronix swaps bytes on a 16-bit boundary when configured in Octal DTR.
    The byte order of 16-bit words is swapped when read or write written in
    8D-8D-8D mode compared to STR modes. Swapping the bytes is a bad design
    decision because this may affect the boot sequence if the entire boot
    sequence is not handled in either 8D-8D-8D mode or 1-1-1 mode. Allow
    operations to specify the byte order in DTR mode, so that controllers can
    swap the bytes back at run-time to fix the endianness, if they are capable.
    
    The byte order in 8D-8D-8D mode can be retrieved at run-time by checking
    BFPT[DWORD(18)] BIT(31). When set to one, the "Byte order of 16-bit words
    is swapped when read in 8D-8D-8D mode compared to 1-1-1 mode.". It doesn't
    specify if this applies to both register and data operations. Macronix is
    the single user of this byte swap and it doesn't have clear rules, as it
    contains register operations that require data swap (RDPASS, WRPASS,
    PASSULK, RDSFDP) and register operations that don't require data swap
    (WRFBR). All these are not common and can be handled in 1-1-1 mode, so we
    can ignore them for now. All the other register operations are done on one
    byte length. The read register operations are actually in 8D-8D-8S mode,
    as they send the data value twice, on each half of the clock cycle. In case
    of a register write of one byte, the memory supports receiving the register
    value only on the first byte, thus it discards the value of the byte on the
    second half of the clock cycle. Swapping the bytes for one byte register
    writes is not required, and for one byte register reads it doesn't matter.
    Thus swap the bytes only for read or page program operations.
    
    Signed-off-by: Tudor Ambarus <tudor.ambarus@microchip.com>
    [eugen.hristev@microchip.com: ported to 2023.01]
    Signed-off-by: Eugen Hristev <eugen.hristev@microchip.com>

--- drivers/mtd/spi/spi-nor-core.c
+++ drivers/mtd/spi/spi-nor-core.c
@@ -275,6 +275,9 @@ void spi_nor_setup_op(const struct spi_nor *nor,
 		op->cmd.dtr = op->addr.dtr = op->dummy.dtr =
 			op->data.dtr = true;
 
+		if (spi_nor_protocol_is_dtr_bswap16(proto))
+			op->data.dtr_bswap16 = true;
+
 		/* 2 bytes per clock cycle in DTR mode. */
 		op->dummy.nbytes *= 2;
 
@@ -473,7 +476,7 @@ static int read_sr(struct spi_nor *nor)
 	u8 val[2];
 	u8 addr_nbytes, dummy;
 
-	if (nor->reg_proto == SNOR_PROTO_8_8_8_DTR) {
+	if (spi_nor_protocol_is_octal_dtr(nor->reg_proto)) {
 		addr_nbytes = nor->rdsr_addr_nbytes;
 		dummy = nor->rdsr_dummy;
 	} else {
@@ -516,7 +519,7 @@ static int read_fsr(struct spi_nor *nor)
 	u8 val[2];
 	u8 addr_nbytes, dummy;
 
-	if (nor->reg_proto == SNOR_PROTO_8_8_8_DTR) {
+	if (spi_nor_protocol_is_octal_dtr(nor->reg_proto)) {
 		addr_nbytes = nor->rdsr_addr_nbytes;
 		dummy = nor->rdsr_dummy;
 	} else {
@@ -3825,8 +3828,8 @@ static int spi_nor_octal_dtr_enable(struct spi_nor *nor)
 	if (!nor->octal_dtr_enable)
 		return 0;
 
-	if (!(nor->read_proto == SNOR_PROTO_8_8_8_DTR &&
-	      nor->write_proto == SNOR_PROTO_8_8_8_DTR))
+	if (!(spi_nor_protocol_is_octal_dtr(nor->read_proto) &&
+	      spi_nor_protocol_is_octal_dtr(nor->write_proto)))
 		return 0;
 
 	if (!(nor->flags & SNOR_F_IO_MODE_EN_VOLATILE))
@@ -4022,6 +4025,18 @@ void spi_nor_set_fixups(struct spi_nor *nor)
 #endif /* SPI_FLASH_MACRONIX */
 }
 
+static void spi_nor_set_dtr_bswap16_ops(struct spi_nor *nor,
+					struct spi_nor_flash_parameter *params)
+{
+	if (params->hwcaps.mask & (SNOR_HWCAPS_READ_8_8_8_DTR |
+				   SNOR_HWCAPS_PP_8_8_8_DTR)) {
+		params->reads[SNOR_CMD_READ_8_8_8_DTR].proto |=
+			SNOR_PROTO_IS_DTR_BSWAP16;
+		params->page_programs[SNOR_CMD_PP_8_8_8_DTR].proto |=
+			SNOR_PROTO_IS_DTR_BSWAP16;
+	}
+}
+
 int spi_nor_scan(struct spi_nor *nor)
 {
 	struct spi_nor_flash_parameter params;
@@ -4113,6 +4128,9 @@ int spi_nor_scan(struct spi_nor *nor)
 
 	if (info->flags & SPI_NOR_SOFT_RESET)
 		nor->flags |= SNOR_F_SOFT_RESET;
+
+	if (nor->flags & SNOR_F_DTR_BSWAP16)
+		spi_nor_set_dtr_bswap16_ops(nor, &params);
 
 	nor->page_size = params.page_size;
 	mtd->writebufsize = nor->page_size;
