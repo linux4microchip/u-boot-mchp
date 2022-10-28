cb1e5f22800b6a1b0a63310ee1fc04f72c9d3d8d
Eugen Hristev <eugen.hristev@microchip.com>
2022-10-28 17:21:41 +0300

    board: sama5d27_som1_ek: guard L2_PL310 definition by ARM_SMCCC
    Enable L2 Cache controller operations only on ARM_SMCCC when OP-TEE is used
    and not in all scenarios.
    
    Signed-off-by: Eugen Hristev <eugen.hristev@microchip.com>
    Reviewed-by: Tudor Ambarus <tudor.ambarus@microchip.com>

--- include/configs/sama5d27_som1_ek.h
+++ include/configs/sama5d27_som1_ek.h
@@ -14,8 +14,14 @@
 #undef CONFIG_SYS_AT91_MAIN_CLOCK
 #define CONFIG_SYS_AT91_MAIN_CLOCK      24000000 /* from 24 MHz crystal */
 
+/*
+ * Only if we have ARM SMC convention call, the L2 cache has to be maintained
+ * by U-boot
+ */
+#ifdef CONFIG_ARM_SMCCC
 #define CONFIG_SYS_L2_PL310
 #define CONFIG_SYS_PL310_BASE		0xa00000
+#endif
 
 #ifdef CONFIG_SPL_BUILD
 #define CONFIG_SYS_INIT_SP_ADDR		0x218000
