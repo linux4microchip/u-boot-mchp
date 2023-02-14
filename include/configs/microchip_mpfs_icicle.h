95cd6498fd757d9d7a3430261081f1857e9d70fb
Padmarao Begari <padmarao.begari@microchip.com>
2023-02-14 15:50:17 +0530

    icicle: Add design overlays script
    Use an env variable(design_overlays) to apply the design
    structure overlays to the fitImage.
    
    Signed-off-by: Padmarao Begari <padmarao.begari@microchip.com>

--- include/configs/microchip_mpfs_icicle.h
+++ include/configs/microchip_mpfs_icicle.h
@@ -47,11 +47,26 @@
 	BOOT_TARGET_DEVICES_MMC(func)\
 	BOOT_TARGET_DEVICES_DHCP(func)
 
+#define BOOTENV_DESIGN_OVERLAYS \
+	"design_overlays=" \
+	"if test -n ${no_of_overlays}; then " \
+		"setenv inc 1; " \
+		"setenv idx 0; " \
+		"fdt resize ${dtbo_size}; " \
+		"while test $idx -ne ${no_of_overlays}; do " \
+			"setenv dtbo_name dtbo_image${idx}; " \
+			"setenv fdt_cmd \"fdt apply $\"$dtbo_name; " \
+			"run fdt_cmd; " \
+			"setexpr idx $inc + $idx; " \
+		"done; " \
+	"fi;\0 " \
+
 #include <config_distro_bootcmd.h>
 
 #define CFG_EXTRA_ENV_SETTINGS \
 	"bootm_size=0x10000000\0" \
 	"scriptaddr=0x8e000000\0" \
+	BOOTENV_DESIGN_OVERLAYS \
 	BOOTENV \
 
 #endif /* __CONFIG_H */
