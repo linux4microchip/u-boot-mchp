3df9986755c082680c12f216707508edcec60687
Valentina Fernandez <valentina.fernandezalanis@microchip.com>
2023-10-10 14:05:29 +0100

    include: configs: icicle: add required env variables for distro boot
    The U-Boot syslinux and pxe boot commands require a number of env
    variables set. Add the mandatory env variables to the icicle kit
    configuration file to support booting through extlinux and similar
    boot mechanism variants.
    
    Signed-off-by: Valentina Fernandez <valentina.fernandezalanis@microchip.com>

--- include/configs/microchip_mpfs_icicle.h
+++ include/configs/microchip_mpfs_icicle.h
@@ -35,12 +35,11 @@
 
 #define CFG_EXTRA_ENV_SETTINGS \
 	"bootm_size=0x10000000\0" \
-	"kernel_addr_r=0x84000000\0" \
-	"fdt_addr_r=0x88000000\0" \
+	"kernel_addr_r=0x80200000\0" \
+	"fdt_addr_r=0x8a000000\0" \
+	"fdtoverlay_addr_r=0x8a080000\0" \
+	"ramdisk_addr_r=0x8aa00000\0" \
 	"scriptaddr=0x8e000000\0" \
-	"pxefile_addr_r=0x88200000\0" \
-	"ramdisk_addr_r=0x88300000\0" \
-	"fdtfile=" CONFIG_DEFAULT_FDT_FILE "\0" \
 	BOOTENV_DESIGN_OVERLAYS \
 	BOOTENV \
 
