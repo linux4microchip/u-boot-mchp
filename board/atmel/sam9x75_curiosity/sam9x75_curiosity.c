ecdb97bf3c8fcffc3bd6097a6862d0db535da8ab
Manikandan Muralidharan <manikandan.m@microchip.com>
2023-10-26 12:22:42 +0530

    board: sam9x75_curiosity: Add support to detect extension boards
    Add support to detect curiosity extension boards such as
    LVDS or MIPI display and Gigabit Ethernet via I2C EEPROM and mark
    the u-boot env appropriately to load their DT-Overlays.
    
    Signed-off-by: Manikandan Muralidharan <manikandan.m@microchip.com>

--- board/atmel/sam9x75_curiosity/sam9x75_curiosity.c
+++ board/atmel/sam9x75_curiosity/sam9x75_curiosity.c
@@ -17,6 +17,12 @@
 #include <init.h>
 #include <linux/delay.h>
 
+#define LVDS_MIPI_DISPLAY_EEPROM	"eeprom@53"
+#define GB_ETH_SODIMM_EEPROM		"eeprom@54"
+
+void at91_ext_board_display_detect(const char *eeprom);
+void at91_ext_board_eth_detect(const char *eeprom);
+
 DECLARE_GLOBAL_DATA_PTR;
 
 void at91_prepare_cpu_var(void);
@@ -24,6 +30,11 @@ void at91_prepare_cpu_var(void);
 int board_late_init(void)
 {
 	at91_prepare_cpu_var();
+
+#ifdef CONFIG_I2C_EEPROM
+	at91_ext_board_display_detect(LVDS_MIPI_DISPLAY_EEPROM);
+	at91_ext_board_eth_detect(GB_ETH_SODIMM_EEPROM);
+#endif
 
 	return 0;
 }
