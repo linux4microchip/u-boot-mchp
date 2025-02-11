d99aa7f6486f6a157d9b497c6e260d365c1e545d
Jamie Gibbons <jamie.gibbons@microchip.com>
2025-02-11 11:35:07 +0000

    board: microchip: mpfs: make ethernets optional
    A given configuration for a board may make either one, or neither of,
    the ethernet ports available to U-Boot. The init code will fail if a mac
    is not present, so move it to an optional approach.
    Node offsets returned by libfdt can contain negative error numbers, so
    the variable type should be "int". As things stand, if the ethernet
    nodes are not found in the early init callback, the if (node < 0) tests
    pass and the code errors out while trying to set the local-mac-address
    for a non-existent node.
    
    Signed-off-by: Jamie Gibbons <jamie.gibbons@microchip.com>
    Signed-off-by: Conor Dooley <conor.dooley@microchip.com>

--- board/microchip/mpfs_icicle/mpfs_icicle.c
+++ board/microchip/mpfs_icicle/mpfs_icicle.c
@@ -40,7 +40,7 @@ int board_early_init_f(void)
 int board_late_init(void)
 {
 	u32 ret;
-	u32 node;
+	int node;
 	u8 idx;
 	u8 device_serial_number[16] = {0};
 	unsigned char mac_addr[6];
@@ -48,18 +48,6 @@ int board_late_init(void)
 	void *blob = (void *)gd->fdt_blob;
 	struct udevice *dev;
 	struct mpfs_sys_serv *sys_serv_priv;
-
-	node = fdt_path_offset(blob, "ethernet0");
-	if (node < 0) {
-		printf("No ethernet0 path offset\n");
-		return -ENODEV;
-	}
-
-	ret = fdtdec_get_byte_array(blob, node, "local-mac-address", mac_addr, 6);
-	if (ret) {
-		printf("No local-mac-address property\n");
-		return -EINVAL;
-	}
 
 	sys_serv_priv = devm_kzalloc(dev, sizeof(*sys_serv_priv), GFP_KERNEL);
 	if (!sys_serv_priv)
@@ -94,10 +82,13 @@ int board_late_init(void)
 	mac_addr[4] = device_serial_number[1];
 	mac_addr[5] = device_serial_number[0];
 
-	ret = fdt_setprop(blob, node, "local-mac-address", mac_addr, 6);
-	if (ret) {
-		printf("Error setting local-mac-address property\n");
-		return -ENODEV;
+	node = fdt_path_offset(blob, "/soc/ethernet@20112000");
+	if (node >= 0) {
+		ret = fdt_setprop(blob, node, "local-mac-address", mac_addr, 6);
+		if (ret) {
+			printf("Error setting local-mac-address property for ethernet@20112000\n");
+			return -ENODEV;
+		}
 	}
 
 	icicle_mac_addr[0] = '[';
@@ -114,6 +105,15 @@ int board_late_init(void)
 	env_set("icicle_mac_addr0", icicle_mac_addr);
 
 	mac_addr[5] = device_serial_number[0] + 1;
+
+	node = fdt_path_offset(blob, "/soc/ethernet@20110000");
+	if (node >= 0) {
+		ret = fdt_setprop(blob, node, "local-mac-address", mac_addr, 6);
+		if (ret) {
+			printf("Error setting local-mac-address property for ethernet@20110000\n");
+			return -ENODEV;
+		}
+	}
 
 	icicle_mac_addr[0] = '[';
 
