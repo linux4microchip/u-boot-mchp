ca6a62d01270b0679443e6d2e2e71c9e36df5535
Jamie Gibbons <jamie.gibbons@microchip.com>
2025-02-11 13:04:28 +0000

    board: microchip: pic64gx_curiosity_kit: make use of ft_board_setup()
    Move ethernet mac address setting to ft_board_setup() to remove the need
    for fdt set in custom boot script.
    
    Signed-off-by: Jamie Gibbons <jamie.gibbons@microchip.com>
    Signed-off-by: Conor Dooley <conor.dooley@microchip.com>

--- board/microchip/pic64gx_curiosity_kit/pic64gx_curiosity_kit.c
+++ board/microchip/pic64gx_curiosity_kit/pic64gx_curiosity_kit.c
@@ -18,6 +18,8 @@ DECLARE_GLOBAL_DATA_PTR;
 #define PIC64GX_SYSREG_SOFT_RESET         ((unsigned int *)0x20002088)
 #define PERIPH_RESET_VALUE		0x800001e8u
 
+static unsigned char mac_addr[6];
+
 int board_init(void)
 {
 	/* For now nothing to do here. */
@@ -41,10 +43,7 @@ int board_late_init(void)
 {
 	u32 ret;
 	int node;
-	u8 idx;
 	u8 device_serial_number[16] = { 0 };
-	unsigned char mac_addr[ARP_HLEN];
-	char pic64gx_curiosity_kit_mac_addr[20];
 	void *blob = (void *)gd->fdt_blob;
 	struct udevice *dev;
 	struct mpfs_sys_serv *sys_serv_priv;
@@ -90,20 +89,25 @@ int board_late_init(void)
 			return -ENODEV;
 		}
 	}
-	pic64gx_curiosity_kit_mac_addr[0] = '[';
-
-	sprintf(&pic64gx_curiosity_kit_mac_addr[1], "%pM", mac_addr);
-
-	pic64gx_curiosity_kit_mac_addr[18] = ']';
-	pic64gx_curiosity_kit_mac_addr[19] = '\0';
-
-	for (idx = 0; idx < 20; idx++) {
-		if (pic64gx_curiosity_kit_mac_addr[idx] == ':')
-			pic64gx_curiosity_kit_mac_addr[idx] = ' ';
-	}
-	env_set("pic64gx_curiosity_kit_mac_addr0", pic64gx_curiosity_kit_mac_addr);
 
 	mpfs_syscontroller_process_dtbo(sys_serv_priv);
+
+	return 0;
+}
+
+int ft_board_setup(void *blob, struct bd_info *bd)
+{
+	u32 ret;
+	int node;
+
+	node = fdt_path_offset(blob, "/soc/ethernet@20110000");
+	if (node >= 0) {
+		ret = fdt_setprop(blob, node, "local-mac-address", mac_addr, 6);
+		if (ret) {
+			printf("Error setting local-mac-address property for ethernet@20110000\n");
+			return -ENODEV;
+		}
+	}
 
 	return 0;
 }
